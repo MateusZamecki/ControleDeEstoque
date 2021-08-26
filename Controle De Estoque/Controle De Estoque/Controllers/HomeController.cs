d4c6645a22388a8d7234518369219501dd9aca49
MateusZamecki <mateuszamecki@hotmail.com>
2021-08-26 00:05:51 -0400

    Finalizando Create

--- Controle De Estoque/Controle De Estoque/Controllers/HomeController.cs
+++ Controle De Estoque/Controle De Estoque/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using Controle_De_Estoque.Models;
+﻿using Controle_De_Estoque.DAL;
+using Controle_De_Estoque.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Logging;
 using ProjetoIntegradorMVC.Models.Repositorio;
 using System;
@@ -12,16 +14,16 @@ namespace Controle_De_Estoque.Controllers
 {
     public class HomeController : Controller
     {
-        private ProdutoService oProdutoService = new ProdutoService();
-        private CategoriaService oCategoriaService = new CategoriaService();
-        private FornecedorService oFornecedorService = new FornecedorService();
+        private ProdutoService oProdutoService = new();
+        private CategoriaService oCategoriaService = new();
+        private FornecedorService oFornecedorService = new();
         public IActionResult Principal()
         {
             List<Produto> produtos = oProdutoService.oRepositorioProduto.SelecionarTodos();
             foreach (var produto in produtos) 
             {
                 var categoriaProduto = oCategoriaService.oRepositorioCategoria.SelecionarPorId(produto.CategoriaId);
-                var fornecedorProduto = oFornecedorService.oRepositorioFornecedor.SelecionarPorId(produto.CategoriaId);
+                var fornecedorProduto = oFornecedorService.oRepositorioFornecedor.SelecionarPorId(produto.FornecedorId);
                 produto.AdicionarCategoria(categoriaProduto);
                 produto.AdicionarFornecedor(fornecedorProduto);
             }
@@ -29,11 +31,22 @@ namespace Controle_De_Estoque.Controllers
         }
         public IActionResult Create()
         {
+            var categorias = oCategoriaService.oRepositorioCategoria.SelecionarTodos();
+            var fornecedores = oFornecedorService.oRepositorioFornecedor.SelecionarTodos();
+            ViewBag.CategoriaId = new SelectList(categorias, "Id","Nome");
+            ViewBag.FornecedorId = new SelectList(fornecedores, "Id", "Nome");
             return View();
         }
         [HttpPost]
         public IActionResult Create(Produto produto)
         {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            produto.ValorCompra /= 100;
+            produto.ValorVenda /= 100;
+            oProdutoService.oRepositorioProduto.Incluir(produto);
             return View();
         }
     }
